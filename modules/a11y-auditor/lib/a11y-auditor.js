0b398973341b8e732f61ddc9f1c03acccbc57907
Michael Spaxman <seanforyou23@gmail.com>
2020-07-17 12:23:54 -0400

    finish configuring lerna for typescript, fix webpack loader paths

--- modules/a11y-auditor/lib/a11y-auditor.js
+++ modules/a11y-auditor/lib/a11y-auditor.js
@@ -1,8 +1,8 @@
 'use strict';
 
-module.exports = a11yAuditor;
-
 function a11yAuditor() {
-    // TODO
-    console.log('called a11yAuditor!');
+  // TODO
+  console.log('called a11yAuditor!');
 }
+
+module.exports = a11yAuditor;
--- modules/host-app/src/app/Dashboard/Dashboard.tsx
+++ modules/host-app/src/app/Dashboard/Dashboard.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { PageSection, Title } from '@patternfly/react-core';
 import { ThresholdComparison } from '@app/Auditor/ThresholdComparison';
-import { a11yAuditor } from '@seanforyou23/a11y-auditor';
-console.log(a11yAuditor());
+import thing from '@seanforyou23/a11y-auditor';
+thing();
 const Dashboard: React.FunctionComponent = () => (
   <PageSection>
     {/* <Title headingLevel="h1" size="lg">Dashboard Page Title</Title> */}
