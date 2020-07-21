2c9b6f88c0c3f4d8dba9401db14416dcd405284c
Michael Spaxman <seanforyou23@gmail.com>
2020-07-21 16:11:45 -0400

    incremental commit for list of links, almost resursive

--- modules/a11y-auditor/lib/crawler2.js
+++ modules/a11y-auditor/lib/crawler2.js
@@ -1,25 +1,51 @@
 const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
-const baseUrl = 'https://www.patternfly.org/v4';
+const baseUrl = 'https://www.draftcab.io';
 const filterNamedAnchors = href => href.indexOf('#') === -1;
 const filterExternalUrls = href => !href.indexOf(baseUrl);
+let linksAlreadyCrawled = [];
+let linksIdentified = [];
+let linksLeftToCrawl = [];
 
 (async () => {
-
   const browser = await puppeteer.launch({ headless: false, slowMo: 250 });
 
-  const page = await browser.newPage();
-  await page.goto(baseUrl);
+  const extractLinks = async (url) => {
+    const page = await browser.newPage();
+    await page.goto(url);
+    console.log('Scraping: ', url);
+    linksAlreadyCrawled.push(url);
+    const linksOnPage = await page.$$eval('a', anchorElements => anchorElements.map(a => a.href)); // all links, including ones we don't want
+    const filteredLinksOnPage = linksOnPage
+      .filter(filterNamedAnchors)
+      .filter(filterExternalUrls)
+      .map(href => href.replace(/\/$/, ''))
+      // .map(href => { return { path: href, id: href.substr(href.lastIndexOf('/')) } });
+    await page.close();
 
-  const getAllPageLinks = await page.$$eval('a', anchorElements => anchorElements.map(a => a.href));
+    linksIdentified.push(...filteredLinksOnPage);
 
-  const filteredLinks = getAllPageLinks
-    .filter(filterNamedAnchors)
-    .filter(filterExternalUrls)
-    .map(href => { return { path: href, id: href.substr(href.lastIndexOf('/')) } })
+    // should we end recursion
+    if (filteredLinksOnPage.length < 1) {
+      return filteredLinksOnPage; // []
+    } else {
+      // go fetch the next page
+      // console.log('filteredLinksOnPage', filteredLinksOnPage);
+      console.log('linksAlreadyCrawled', linksAlreadyCrawled);
+      console.log('linksIdentified', linksIdentified);
 
-  console.log(filteredLinks);
+      linksLeftToCrawl = linksIdentified.filter(link => !linksAlreadyCrawled.includes(link));
+      console.log('linksLeftToCrawl', linksLeftToCrawl);
+      // what is the next url?
+      const nextPage = linksLeftToCrawl[0];
+      console.log('nextPage', nextPage);
+    }
+
+    return filteredLinksOnPage;
+  }
+
+  const links = await extractLinks(baseUrl);
 
   await browser.close();
 
